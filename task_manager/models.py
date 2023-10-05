9f50342ba07bc3df7a88d418aa01bbc04ec7f1c7
geekyxamit <sahuamit0327@gmail.com>
2023-10-06 01:24:53 +0530

    made required models and serializers

---
+++ task_manager/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, null=True)
+    age = models.IntegerField(null=True)
+
+class Tasks(models.Model):
+    task_title = models.CharField(max_length=200)
+    task_description = models.TextField()
+    due_date = models.DateField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="task_creator")
+    assignee = models.ForeignKey(User, on_delete=models.CASCADE, related_name="task_assignee")
