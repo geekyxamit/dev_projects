0a2623056e8ea55020106e3503ba328dcffe5a17
geekyxamit <sahuamit0327@gmail.com>
2023-10-06 23:50:05 +0530

    post method to create task with unit testing checks

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     # path('', views.index, name = 'home'),
     path('users/', CreateAndViewUser.as_view(), name="users_data"),
+    path('tasks/', CreateAndViewTask.as_view(), name="tasks_data"),
     
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import request
 from .serializers import *
+import datetime
 
 # Create your views here.
 
@@ -45,3 +46,59 @@ class CreateAndViewUser(APIView):
         return Response({"message": "User created successfully",
                          "success": True})
     
+
+
+class CreateAndViewTask(APIView):
+    
+    # method to view tasks
+    def get(self, request, *args, **kwargs):
+        pass
+    
+    # method to create a task
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        
+        if data.get("task_title") is None or data.get("task_title").strip() == "":
+            return Response({"error": "Task Title not given",
+                             "success": False})
+        
+        if data.get("task_description") is None or data.get("task_description").strip() == "":
+            return Response({"error": "Task Description not given",
+                             "success": False})
+            
+        if data.get("due_date") is None:
+            return Response({"error": "Task due date not given",
+                             "success": False})
+        
+        if data.get("assignee_ids") is None:
+            return Response({"error": "Task assignee IDs not given",
+                             "success": False})
+        
+        if data.get("created_by") is None:
+            return Response({"error": "Task creator not given",
+                             "success": False})
+        
+        data["due_date"] = datetime.datetime.strptime(data["due_date"], "%Y-%m-%d %H:%M:%S").date()
+        
+        unassigned_id_list = []
+        
+        for assignee_id in data["assignee_ids"]:
+            data["assignee"] = assignee_id
+            # searching assignee user in DB
+            assignee_obj  = User.objects.filter(id=assignee_id).first()
+            
+            if assignee_obj is None:
+                # if assignee with given id not found then skip this id
+                unassigned_id_list.append(assignee_id)
+                continue
+            
+            ser_data = TasksSerializer(data=data)
+            if ser_data.is_valid():
+                ser_data.save()
+            else:
+                # if serializer is invalid, then akip this id
+                unassigned_id_list.append(assignee_id)
+                
+        return Response({"message": "Task created successfully",
+                         "unassigned_ids": unassigned_id_list,
+                         "success": True})
